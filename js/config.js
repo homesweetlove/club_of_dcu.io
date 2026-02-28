348808d8962b67abd9102c2b940661c7581d7e68
homesweetlove <zlem8952@cu.ac.kr>
2026-03-01 00:54:20 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// js/config.js
+// 사이트 기본 설정
+export const CONFIG = {
+  SITE_NAME: "동아리 포털",
+  DATA_PATH: "./data/clubs.json", // 같은 레포 내 상대경로
+  SUBMIT_LINK: "", // 예: "https://forms.gle/..." 또는 GitHub Issues 링크
+  SUBMIT_LINK_LABEL: "동아리 등록/수정 요청하기"
+};
